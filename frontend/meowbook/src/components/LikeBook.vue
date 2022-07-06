b01018b240c08aee7f6bc72d738d08fa6031d833
AaronCS25 <aaron.camacho@utec.edu.pe>
2022-07-06 18:57:10 -0500

    app.vue and LikeBook.vue modified

--- frontend/meowbook/src/components/LikeBook.vue
+++ frontend/meowbook/src/components/LikeBook.vue
@@ -1,11 +1,18 @@
 <template>
   <li>
     <h3>Título</h3>
+    <ul>
+      <li>Autor:</li>
+      <li>Sinopsis:</li>
+      <li>Editorial</li>
+      <li>Fecha de publicación:</li>
+      <li>ISBN:</li>
+    </ul>
   </li>
 </template>
 
 <script>
 export default {
-    name: "book"
+  name: "book",
 };
 </script>
