f1bc7328430f7912f943298cd63738a83615732c
Cristobal135 <103542038+Cristobal135@users.noreply.github.com>
2022-07-05 22:53:43 -0500

    Login (versión temprana)

--- frontend/meowbook/src/views/Login.vue
+++ frontend/meowbook/src/views/Login.vue
@@ -1,38 +1,55 @@
 <template>
-  <html>
-    <body>
-      <form action="/login/succesful" class="login" method="POST">
-        <h1>Iniciar Sesión</h1>
-        <div class="container">
-          <div class="input-container">
-            <i class="fa-solid fa-envelope icon"></i>
-            <input
-              id="email"
-              required
-              name="email"
-              type="email"
-              placeholder="Email@"
-            />
-          </div>
-          <div class="input-container">
-            <i class="fa-solid fa-key icon"></i>
-            <input
-              id="contraseña"
-              required
-              name="contraseña"
-              type="password"
-              placeholder="Contraseña"
-            />
-          </div>
-
-          <p>{{ data }}</p>
-        </div>
-        <input type="submit" value="Log In" class="button" />
-        <p>
-          ¿Aún no tienes una cuenta MeowBook (=^･ｪ･^=)?
-          <router-link to="/signin">Sign In</router-link>
-        </p>
-      </form>
-    </body>
-  </html>
+  <div>
+    <h1>Iniciar Sesión</h1>
+    <form @submit.prevent="login">
+      <input v-model="nombre" placeholder="nombre" /><br />
+      <input
+        v-model="contrasena"
+        type="password"
+        placeholder="contraseña"
+      /><br />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+  <p>
+    ¿Aún no tienes una cuenta MeowBook (=^･ｪ･^=)?
+    <router-link to="/signin">Sign In</router-link>
+  </p>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      nombre: "",
+      contrasena: "",
+    };
+  },
+  methods: {
+    async login() {
+      const url = "http://127.0.0.1:5000/";
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          nombre: this.nombre,
+          contrasena: this.contrasena,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.groupCollapsed("response: ", response);
+      const data = await response.json();
+      console.log("data: ", data);
+      if (data["success"]) {
+        this.$router.push({
+          name: "Home",
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
