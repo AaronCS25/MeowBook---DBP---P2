fa4bc9f33d6c6dc83723f05000f84deacaa32ada
AaronCS25 <aaron.camacho@utec.edu.pe>
2022-07-06 23:50:56 -0500

    something updated

--- frontend/meowbook/src/views/Home.vue
+++ frontend/meowbook/src/views/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <h1>Home</h1>
-    <input type="text" t-model="search">
-    <p>{{search}}</p>
+    <input type="text" v-model="search" />
+    <button type="submit">search</button>
+    <ul style="border: 1px solid black">
+      <li class="libro">
+        <h4>Título Libro</h4>
+        <ul class="conjunto-libros">
+          <li class="elemento-libro">Autor: -------------------</li>
+          <li class="elemento-libro">Publicación: -------------</li>
+          <li class="elemento-libro">Editorial: ----------------</li>
+          <li class="elemento-libro">ISBN: ---------------------</li>
+        </ul>
+      </li>
+    </ul>
+    <p>{{ search }}</p>
   </div>
 </template>
 
@@ -11,8 +23,27 @@ export default {
   name: "Home",
   data() {
     return {
-      search: ''
-    }
-  }
+      search: "",
+    };
+  },
 };
 </script>
+
+<style>
+.libro {
+  display: flex;
+  text-decoration: none;
+}
+
+.conjunto-libro {
+  padding: 2px;
+  text-decoration: none;
+  justify-content: left;
+}
+
+.elemento-libro {
+  padding: 2px;
+  text-decoration: none;
+  justify-content: left;
+}
+</style>
