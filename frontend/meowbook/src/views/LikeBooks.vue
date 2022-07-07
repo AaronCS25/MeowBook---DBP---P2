6910264d3350baffbc6ebc532bd0573d0210b2bc
AaronCS25 <aaron.camacho@utec.edu.pe>
2022-07-06 19:58:44 -0500

    LikeBooks.vue view modified

--- frontend/meowbook/src/views/LikeBooks.vue
+++ frontend/meowbook/src/views/LikeBooks.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <h1>LikeBooks</h1>
-    <ul>
-      <li>Título</li>
+    <ul class="libros">
+      <li class="libro">
+        <h4>Título</h4>
+        <ul class="especificaciones">
+          <li class="elemento">Autor:</li>
+          <li class="elemento">Publicación:</li>
+          <li class="elemento">ISBN:</li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
@@ -12,3 +19,25 @@ export default {
   name: "LikeBooks",
 };
 </script>
+
+<style>
+.libros {
+  border: 1px solid black;
+  text-align: justify;
+  display: flex;
+}
+
+.libro {
+  border: 1px solid red;
+  list-style: none;
+}
+.especificaciones {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+}
+.elemento {
+  padding: 5px;
+  border: 1px solid black;
+}
+</style>
