095d60c18fd49ab2007bd5d5a79fbe531d2ef883
jeffHQ <103541640+jeffHQ@users.noreply.github.com>
2022-07-12 16:47:27 -0500

    UPDATE FINAL

--- backend/test_meowbook.py
+++ backend/test_meowbook.py
@@ -41,7 +41,7 @@ class TestCaseTodoApp(unittest.TestCase):
 
 
 #--------------------Autor_Test---------------------#
-    @unittest.skip('TEST PASS')
+    
     def test_get_autor_successfully(self):
         res = self.client().get('/autores')
         data = json.loads(res.data)
@@ -51,7 +51,6 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertTrue(data['autores'])
         self.assertTrue(data['total_autoress'])
 
-    @unittest.skip('TEST PASS')
     def test_get_autor_unsuccessfully(self):
         res = self.client().get('/autoress')
         data = json.loads(res.data)
@@ -60,7 +59,6 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertTrue(data['message'])
 
-    @unittest.skip('TEST PASS')
     def test_create_autor_successfully(self):
         res = self.client().post('/autores', json = self.newAutor)
         data = json.loads(res.data)
@@ -70,7 +68,6 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertTrue(data['autores'])
         self.assertTrue(data['total_autores'])
 
-    @unittest.skip('TEST PASS')
     def test_create_autor_unsuccessfully(self):
         res = self.client().post('/autores', json = {})
         data = json.loads(res.data)
@@ -80,7 +77,6 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(data['message'], 'unprocessable')
         self.assertTrue(data['message'])
 
-    @unittest.skip('TEST PASS')
     def test_get_autor_by_nombre_successfully(self):
         res = self.client().post('/autores', json = self.newAutor_search)
         data = json.loads(res.data)
@@ -88,9 +84,8 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['autores'][0]['autor_nombre'], 'Mario' )
-        pass
+        
 
-    @unittest.skip('TEST PASS')
     def test_get_autor_by_nombre_empty(self):
         res = self.client().post('/autores', json = {'search': '0'})
         data = json.loads(res.data)
@@ -98,9 +93,8 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['autores'], [] )
-        pass
-
-    @unittest.skip('TEST PASS')    
+        
+    @unittest.skip('TEST PASS')
     def test_update_autores_by_id_successfully(self):
         res = self.client().patch('/autores/1', json = {'autor_nombre': 'update_nombre'})
         data = json.loads(res.data)
@@ -108,9 +102,8 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['autor_id'], '1')
-        pass
+        
 
-    @unittest.skip('TEST PASS') 
     def test_update_autores_by_id_unsuccessfully(self):
         res = self.client().patch('/autores/-1', json = {'autor_nombre': 'update_nombre'})
         data = json.loads(res.data)
@@ -119,16 +112,15 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertTrue(data['message'])
 
-    @unittest.skip('TEST PASS') 
+    @unittest.skip('TEST PASS')
     def test_delete_autores_by_id_successfully(self):
         res = self.client().delete('/autores/10')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        pass
-
-    @unittest.skip('TEST PASS') 
+    
+    @unittest.skip('TEST PASS')
     def test_delete_autores_by_id_unsuccessfully(self):
         res = self.client().delete('/autores/-10')
         data = json.loads(res.data)
@@ -136,66 +128,41 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], False)
         self.assertTrue(data['message'])
-        pass
+        
 
 #--------------------Libros_Test--------------------#
-    @unittest.skip('TEST PASS') 
     def test_get_libros_successfully(self):
         res = self.client().get('/libros')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        pass
+        
 
-    @unittest.skip('TEST PASS') 
     def test_get_libros_unsuccessfully(self):
         res = self.client().get('/libross')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
-        pass
         
 
-    @unittest.skip('TEST PASS') 
     def test_create_libros_successfully(self):
         res = self.client().post('/libros', json=self.newLibro)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
+        
         
 
-    @unittest.skip('TEST PASS') 
     def test_create_libros_unsuccessfully(self):
         res = self.client().post('/libros', json={})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
-        pass
-
-    @unittest.skip('TEST PASS') 
-    def test_search_libros_by_id_successfully(self):
-        res = self.client().post('/libros/1', json = self.newAutor_search)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(data['autores'][0]['autor_nombre'], 'Mario' )
-        pass
-
-    @unittest.skip('TEST PASS') 
-    def test_search_libros_by_id_unsuccessfully(self):
-        res = self.client().post('/libros/1', json = self.newAutor_search)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(data['autores'][0]['autor_nombre'], 'Mario' )
-        pass
-
-    @unittest.skip('TEST PASS') 
+        
+        
+    @unittest.skip('TEST PASS')
     def test_update_libros_by_id_successfully(self):
         res = self.client().patch('/libros/1', json = {'autor_nombre': 'update_nombre'})
         data = json.loads(res.data)
@@ -203,85 +170,76 @@ class TestCaseTodoApp(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['autor_id'], '1')
-        pass
+        
 
-    @unittest.skip('TEST PASS') 
     def test_update_libros_by_id_unsuccessfully(self):
         res = self.client().patch('/libros/-1', json = {})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
-        pass
         
-
-    @unittest.skip('TEST PASS') 
+        
+    @unittest.skip('TEST PASS')
     def test_delete_libros_by_id_successfully(self):
         res = self.client().delete('/libros/3')
         data = json.loads(res.data)
-        print('data: ', data)
+        
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        pass
+        
         
 
-    @unittest.skip('TEST PASS') 
     def test_delete_libros_by_id_unsuccessfully(self):
         res = self.client().delete('/libros/-3')
         data = json.loads(res.data)
         print('data: ', data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
-        pass
+        
 
-    @unittest.skip('TEST PASS') 
     def test_get_like_successfully(self):
         res = self.client().get('/likes')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
+        
         
 
-    @unittest.skip('TEST PASS')
     def test_get_like_unsuccessfully(self):
         res = self.client().get('/likess')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
-        pass
+        
 
-    @unittest.skip('TEST PASS')
     def test_search_like_successfully(self):
         res = self.client().get('/likes')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
-
-    @unittest.skip('TEST PASS')
+        
+    
     def test_search_like_unsuccessfully(self):
         res = self.client().get('/likess')
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code, 200)
-        pass
+        self.assertEqual(res.status_code, 404)
+        
 
-    @unittest.skip('TEST PASS')
     def test_create_like_successfully(self):
         res = self.client().post('/libros', json=self.newLibro)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
+        
 
-    @unittest.skip('TEST PASS')
     def test_create_like_unsuccessfully(self):
         res = self.client().post('/libros', json=self.newLibro)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
+        
     
     @unittest.skip('TEST PASS')
     def test_delete_like_successfully(self):
@@ -289,15 +247,15 @@ class TestCaseTodoApp(unittest.TestCase):
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        pass
+        
 
-    @unittest.skip('TEST PASS')
+    
     def test_delete_like_unsuccessfully(self):
         res = self.client().delete('/likes/-2')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
-        pass
+        
 
     def tearDown(self):
         pass
